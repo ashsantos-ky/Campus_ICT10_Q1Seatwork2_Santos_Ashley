461b3dec4f16d0d86b23cc52a6c1d6659ca18e42
ashsantos-ky <santos.ay@obmontessori.edu.ph>
2025-09-04 11:31:09 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from pyscript import display
+
+
+restaurant_name = 'Good dough'
+
+owner_name = "Kirsten Santos"
+
+year_established = 2025
+
+popular_item_price = 9.99
+
+has_delivery = True
+
+products = ['pizza', 'pasta', 'chicken']
+
+business_hours = ['9am-9pm']
+
+menu_prices = ['10.30', '8.50', '7.25']
+
+common_allergens = ("gluten", "dairy", "nuts")
+
+tax_rate = 1.05
+
+display(f"{restaurant_name}!")
+display(f"Owned by: {owner_name}")
+display(f"Finished Cooking this {year_established}")
