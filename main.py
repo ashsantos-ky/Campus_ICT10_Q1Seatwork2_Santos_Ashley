b69c8a5f4eeecc737bafbeb58915abb504d962f1
ashsantos-ky <santos.ay@obmontessori.edu.ph>
2025-09-04 16:29:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 from pyscript import display
 
-
+#intro to my restaurant
 restaurant_name = 'Good dough'
-
-owner_name = "Kirsten Santos"
-
+owner_name = 'Kirsten Santos'
 year_established = 2025
 
+#pricing the pizza
 popular_item_price = 9.99
 
+# delivery option
 has_delivery = True
 
+#other important details
 products = ['pizza', 'pasta', 'chicken']
-
 business_hours = ['9am-9pm']
-
 menu_prices = ['10.30', '8.50', '7.25']
-
 common_allergens = ("gluten", "dairy", "nuts")
-
 tax_rate = 1.05
 
+# to display the information
 display(f"{restaurant_name}!")
 display(f"Owned by: {owner_name}")
 display(f"Finished Cooking this {year_established}")
